d6ec69b87d3f9a51b896969c60dbae683218e5fa
Elika N <153571556+blissels@users.noreply.github.com>
2025-06-11 14:59:03 +0700

    update nama doang biar ga lupa :D

--- frontend/resources/views/admin/layouts/sidebar.blade.php
+++ frontend/resources/views/admin/layouts/sidebar.blade.php
@@ -47,7 +47,7 @@
                     </svg>
                 </span>
             </span>
-            <span class="app-brand-text demo menu-text fw-bold ms-2">Sneat</span>
+            <span class="app-brand-text demo menu-text fw-bold ms-2">EvenTalk</span>
         </a>
 
         <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
